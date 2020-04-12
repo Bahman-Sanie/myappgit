741d0234020aed4955754181372383a5c7f8a6ba
Bahman Sanie <rayanpeyk.man@gmail.com>
2020-04-12 15:22:16 +0200

    changed and.vhd

---
+++ and.vhd
@@ -0,0 +1,16 @@
+-- OCH
+Library ieee;
+Use ieee.std_logic_1164.ALL;
+Use ieee.std_logic_unsigned.ALL;
+
+Entity  AND2  is
+	Generic  (DLY:time:=0  ns);
+	Port  ( A , B :  in  std_logic;
+	           C  :  out  std_logic  );
+End ;
+
+Architecture  OCH  of AND2 is
+
+Begin
+	C  <=  A  and  B after DLY;
+End OCH;
